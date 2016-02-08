75c1bce562500cedc650a09ae8387e793dd16793
venkat <vikkii89@gmail.com>
2016-02-08 12:45:14 -0800

    controlling key board ios

--- ControllingKeyBoard/ViewController.swift
+++ ControllingKeyBoard/ViewController.swift
@@ -8,16 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITextFieldDelegate{
 
+    @IBAction func updateText(sender: AnyObject) {
+        
+        let inp = String(inputVal.text!)
+        print("originalval = \(inputVal)")
+        print("value - \(inp)")
+        if (!inp.isEmpty) {
+            print("inside if")
+            resultText.text = inputVal.text
+        }else{
+            print("inside else")
+           resultText.text = "Please enter something!"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.inputVal.delegate = self
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var resultText: UILabel!
+    @IBOutlet weak var inputVal: UITextField!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 
 
